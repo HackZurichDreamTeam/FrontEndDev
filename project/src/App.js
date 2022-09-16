da0a0dd5cd22da2f55790ce8c0e6408234839300
Alex Bacallao <Alex@b-enterprises.net>
2022-09-17 01:33:08 +0200

    Start of working prototype of map

--- project/src/App.js
+++ project/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+  
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            defaultImage:
+'https://upload.wikimedia.org/wikipedia/commons/f/f7/Europe_laea_location_map.svg',
+        };
+    }
+  
+    // replace image function
+    replaceImage = (error) => {
+        //replacement of broken Image
+        error.target.src = this.state.defaultImage; 
+    }
+  
+    render() {
+        return (
+            <div style={{ width: "0px" }}>
+  
+                <div className='imageDiv'
+                    style={{ border: '1px', 
+                        margin: "10px", }}>
+                    <img
+                        src=""
+                        alt="You broke it"
+                        onError={this.replaceImage}
+                        width='1600px'
+                        height='900px'
+                    />
+                </div>
+  
+            </div>
+        );
+    }
 }
-
+  
 export default App;
